862bae7ec34505d0d1e207f0bfe81f51e8fed005
Vladimir Nguyen <95983775+vladonguyen@users.noreply.github.com>
2024-11-16 22:32:51 +0200

    Get all posts added to backend

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,25 @@ const {v4:generateId} = require("uuid");
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
+//setHeader very important!!!
+
+//CREATE STORE VARIABLE
+let posts = [];
+
+//GET ALL POSTS
+app.get("/api/posts", (req,res,next)=>{
+    try {
+        res.json(posts);
+    } catch (error) {
+        next(error);
+    }
+})
+
+
 app.listen(8080, ()=> console.log("Backend server started at port 8080. Write to express variable"));
 
